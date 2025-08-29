26d50b8e11657e026dd27b29cc7cda6746096aad
PragyanVerma1 <pragyanverma91@gmail.com>
2025-08-29 15:54:10 +0530

    Add files via upload

---
+++ DAY1 (2).c
@@ -0,0 +1,32 @@
+//Write a program to input two numbers and display their sum.
+/*e<stdio.h>
+int main() {
+    int num1;
+    int num2;
+    printf("Enter number 1: ");
+    scanf("%d", &num1);
+    printf("Enter number 2: ");
+    scanf("%d", &num2);
+    int sum = num1 + num2;
+    printf("Sum of two numbers is %d", sum); 
+    return 0;
+
+}*/
+//Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+
+/*#include<stdio.h>
+int main() {
+    int a, b;
+    printf("Enter the value of a : ");
+    scanf("%d", &a);
+    printf("Enter the value of b : ");
+    scanf("%d", &b);
+    printf("The sum is %d", a+b 
+    );
+    printf("The subtraction is %d", a-b);
+    printf("THe multiplication is %d", a*b);
+    float div = a/b;
+    printf("The division is %f", div);
+    return 0;
+} */
