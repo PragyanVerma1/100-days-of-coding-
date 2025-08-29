bfe024c60f96e476638a99c02c9c3749ed3b712c
PragyanVerma1 <pragyanverma91@gmail.com>
2025-08-29 15:55:51 +0530

    Add files via upload

---
+++ DAY3.C
@@ -0,0 +1,31 @@
+//Write a program to convert temperature from Celsius to Fahrenheit.
+//formula: F = (C * 9/5) + 32. 
+/*#include<stdio.h>
+int main() {
+	float c;
+	printf("Enter value of c : ");
+	scanf("%f", &c);
+	float F= (c*(9/5)) + 32;
+	printf("The temp in fahrenhiet is %f", F);
+	return 0;
+}*/
+//Write a program to swap two numbers using a third variable.
+/*#include <stdio.h>
+int main() {
+   int a, b, temp;
+   // Input two numbers
+   printf("Enter the first number: ");
+   scanf("%d", &a);
+   printf("Enter the second number: ");
+   scanf("%d", &b);
+   // Display numbers before swapping
+   printf("\nBefore swapping: a = %d, b = %d\n", a, b);
+   // Swapping logic using a third variable
+   temp = a;
+   a = b;
+   b = temp;
+   // Display numbers after swapping
+   printf("After swapping: a = %d, b = %d\n", a, b);
+   return 0;
+
+}*/
