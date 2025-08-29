0ec2803f51e58ab098fd04953db4fb2196091ab0
PragyanVerma1 <pragyanverma91@gmail.com>
2025-08-29 15:56:23 +0530

    Add files via upload

---
+++ DAY4.C
@@ -0,0 +1,40 @@
+//Write a program to swap two numbers without using a third variable.
+/*#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
+}*/
+//Write a program to find and display the sum of the first n natural numbers.
+
+
+/*#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+  
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+  
+    printf("The sum of the first %d natural numbers is: %d\n", n, sum);
+
+    return 0;
+}*/
+
