bc9117a8e1d7b023cb2da6fd2cae5065251810cc
PragyanVerma1 <pragyanverma91@gmail.com>
2025-08-29 15:55:27 +0530

    Add files via upload

---
+++ DAY2.C
@@ -0,0 +1,27 @@
+//Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
+
+/*#include<stdio.h>
+int main(){
+    int a, b;
+    printf("Enter length : ");
+    scanf("%d", &a);
+    printf("Enter width : ");
+    scanf("%d", &b);
+    int area = a*b;
+    printf("The area of rectangle is %d", area);
+    int perimeter= 2*(a+b);
+    printf("\nThe perimeter is  %d", perimeter);
+    return 0;
+}*/
+//Write a program to calculate the area and circumference of a circle given its radius.
+/*#include<stdio.h>
+int main() {
+    float radius;
+    printf("Enter radius of circle: ");
+    scanf("%f", &radius);
+    float area = 3.14 * radius * radius;
+    printf("Area of circle is %f", area);
+    return 0;
+
+}*/
+
