483129b7bb74fea4a925aa87fae681ea2324b292
PragyanVerma1 <pragyanverma91@gmail.com>
2025-09-13 19:31:34 +0530

    Day 15.c

---
+++ Day 15.c
@@ -0,0 +1,39 @@
+//Write a program to calculate the factorial of a number.
+#include <stdio.h>
+int main() {
+    int n, i;
+    int factorial = 1;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    if (n < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        for (i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        printf("Factorial of %d is %d\n", n, factorial);
+    }
+    return 0;
+}
+
+
+
+//Write a program to reverse a given number
+#include <stdio.h>
+
+int main() {
+    int num, reversed = 0, remainder;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num = num / 10;
+    }
+
+    printf("Reversed number: %d\n", reversed);
+
+    return 0;
+}
