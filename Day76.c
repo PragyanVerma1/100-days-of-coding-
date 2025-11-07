13122a400b2300572b04ca476f651e4bb05c1cef
PragyanVerma1 <pragyanverma91@gmail.com>
2025-11-07 23:02:30 +0530

    Create Day76.c

---
+++ Day76.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char filename[100];
+    char line[256];
+
+    printf("Enter filename: ");
+    scanf("%s", filename);
+
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Error: File does not exist or cannot be opened.\n");
+        return 1;
+    }
+
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        printf("%s", line);
+    }
+
+    fclose(fp);
+    return 0;
+}
