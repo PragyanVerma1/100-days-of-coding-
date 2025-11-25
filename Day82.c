0cc7ba79e64b22be1cfd2a100a9c242ed1a37661
PragyanVerma1 <pragyanverma91@gmail.com>
2025-11-25 18:13:54 +0530

    Create Day82.c

---
+++ Day82.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+enum TrafficLight { RED, YELLOW, GREEN };
+
+int main() {
+    enum TrafficLight signal;
+    signal = RED;  
+
+    if (signal == RED)
+        printf("Stop\n");
+    else if (signal == YELLOW)
+        printf("Wait\n");
+    else if (signal == GREEN)
+        printf("Go\n");
+
+    return 0;
+}
