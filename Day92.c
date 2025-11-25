d880948d80237737026b294b03e5a9674d107642
PragyanVerma1 <pragyanverma91@gmail.com>
2025-11-25 18:24:31 +0530

    Create Day92.c

---
+++ Day92.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+int main() {
+    struct Student students[5];
+
+    for (int i = 0; i < 5; i++) {
+        printf("Enter details for student %d:\n", i + 1);
+        printf("Name: ");
+        getchar();  // Clear newline character from input buffer
+        fgets(students[i].name, sizeof(students[i].name), stdin);
+
+        printf("Roll No: ");
+        scanf("%d", &students[i].roll_no);
+
+        printf("Marks: ");
+        scanf("%f", &students[i].marks);
+    }
+
+    printf("\nStudent Details:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Student %d:\n", i + 1);
+        printf("Name: %s", students[i].name);
+        printf("Roll No: %d\n", students[i].roll_no);
+        printf("Marks: %.2f\n", students[i].marks);
+    }
+
+    return 0;
+}
